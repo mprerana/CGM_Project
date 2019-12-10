965128b214d0abe28ff43f642469722b953b3419
RajatSingh95 <rajat.s15@iiits.in>
2019-12-10 15:54:34 +0530

    Group 3 project files added

---
+++ Group_3/Environment Tour/Assets/LookWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookWalk : MonoBehaviour
+{
+    public Transform vrCamera;
+    public float toggleAngle = 30.0f;
+	public float speed = 8.0f;
+	public float gravity = 10f;
+    public bool moveForward;
+	public CharacterController controller;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PlayerMovement();
+        if (vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x <= 90.0f){
+            moveForward = true;
+        }
+        else{
+            moveForward = false;
+        }
+        if (moveForward){
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+
+    }
+
+}
